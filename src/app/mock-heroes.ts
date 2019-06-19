0268ce099a86cefc12d15ec2552dc6e2e271e55c
laurencormier <cormierlauren14@gmail.com>
2019-06-19 10:15:07 -0400

    app with a list of 3 heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './Hero.class';
+
+export const HEROES: Hero[] = [
+    {id: 15, name: "Sloth Saver"},
+    {id: 32, name: "Goose Chrusher"},
+    {id: 67, name: "Mac and Cheese Man"}
+
+];
